3932028f687eb5932b943d1eb2aca549dff04953
CuongLQT <cuonglqt@mti-tech.vn>
2020-06-05 18:01:14 +0700

    Update Search UI

--- HPOrderList/ViewControllers/HomeVC.swift
+++ HPOrderList/ViewControllers/HomeVC.swift
@@ -22,11 +22,42 @@ class HomeVC: BaseVC {
         }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupSearchBar()
+    }
+    
     override func viewDidLoad() {
         title = "home_title".localized()
         super.viewDidLoad()
         CommonUtil.addRightBtn(to: self, imageNamed: "plus-icon", with: #selector(self.rightBtnAction))
         tbUserlist.separatorColor = UIColor.borderColor
+        
+        
+    }
+    
+    func setupSearchBar(){
+        //SearchVC
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Tìm khách hàng"
+        searchController.searchBar.tintColor = .white
+        searchController.searchBar.barTintColor = .white
+        
+        searchController.searchBar.searchTextField.backgroundColor = UIColor.darkGray.withAlphaComponent(0.5)
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.5) {
+            searchController.searchBar.searchTextField.attributedPlaceholder = NSAttributedString(string:"Tìm khách hàng", attributes:  [NSAttributedString.Key.foregroundColor: UIColor.white.withAlphaComponent(0.75)])
+        }
+        if let textFieldInsideSearchBar = searchController.searchBar.value(forKey: "searchField") as? UITextField,
+            let glassIconView = textFieldInsideSearchBar.leftView as? UIImageView {
+            glassIconView.image = glassIconView.image?.withRenderingMode(.alwaysTemplate)
+            glassIconView.tintColor = UIColor.white.withAlphaComponent(0.75)
+        }
+        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).defaultTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        definesPresentationContext = true
     }
     
     @objc func rightBtnAction() {
@@ -62,4 +93,10 @@ extension HomeVC: UITableViewDelegate, UITableViewDataSource {
         
         return UISwipeActionsConfiguration(actions: [askAction])
     }
+}
+
+extension HomeVC: UISearchResultsUpdating {
+  func updateSearchResults(for searchController: UISearchController) {
+    // TODO
+  }
 }
