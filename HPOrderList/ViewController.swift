25a4093acdf7d9fe5ba8feeef3a2a31a37632c4b
CuongLQT <cuonglqt@mti-tech.vn>
2020-06-05 14:58:41 +0700

    Init Project

---
+++ HPOrderList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HPOrderList
+//
+//  Created by Le Quang Tuan Cuong(CuongLQT) on 6/2/20.
+//  Copyright © 2020 Cuong Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
